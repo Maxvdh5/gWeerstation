b4965091adeb4a7e0b99190181a4d6523e462eef
Terence <tgmbeijl@avans.nl>
2018-09-13 12:59:02 +0200

    Class uart

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include "main.h"
+#include "UART.hpp"
+#include <stdlib.h>
+
+void SystemClock_Config(void) {
+
+	RCC_ClkInitTypeDef RCC_ClkInitStruct;
+	RCC_OscInitTypeDef RCC_OscInitStruct;
+
+	/* No HSE Oscillator on Nucleo, Activate PLL with HSI/2 as source */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_NONE;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+	RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
+	RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		/* Initialization Error */
+		while (1)
+			;
+	}
+
+	/* Select PLL as system clock source and configure the HCLK, PCLK1 clocks dividers */
+	RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK
+			| RCC_CLOCKTYPE_PCLK1);
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK) {
+		/* Initialization Error */
+		while (1)
+			;
+	}
+}
+
+int main() {
+	uint8_t data[5];
+	int size = sizeof(data) / sizeof(data[0]);
+
+	HAL_Init();
+	SystemClock_Config();
+
+	UART uart;
+
+	uart.init(9600);
+	uart.transmit(data, size);
+
+	return 0;
+}
