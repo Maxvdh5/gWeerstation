dcbefd040936037aa44d67179f710d1e8c812074
Pelle van Gils <pelle@pollop.nl>
2018-09-19 18:19:03 +0200

    read BMP180 sensor
    dirty hack to get 'something' from the bmp180 sensor

--- Src/sensors.c
+++ Src/sensors.c
@@ -6,6 +6,7 @@
  */
 
 #include "sensors.h"
+#include "cmsis_os.h"
 
 uint16_t rawHumidity = 0x0000;
 uint16_t rawTemperature = 0x0000;
@@ -76,11 +77,40 @@ int readSensor(uint8_t address, uint8_t command, uint16_t *data)
 	return 0;
 }
 
+int readBMPSensor(uint8_t address, uint8_t command, uint16_t *data)
+{
+	uint8_t measHum[2] = { 0xF4, 0x34 };
+	uint8_t getHum = 0xF6;
+	uint16_t returnData;
+
+	// Request reading
+	if (HAL_I2C_Master_Transmit(&hi2c1, (address << 1), measHum, 2,
+			1000) != HAL_OK)
+		return (1);
+
+	osDelay(20);
+
+	if (HAL_I2C_Master_Transmit(&hi2c1, (address << 1), &getHum, 2,
+			1000) != HAL_OK)
+		return (1);
+
+	// Receive reading
+	if (HAL_I2C_Master_Receive(&hi2c1, (address << 1), (uint8_t *)&returnData, 2, 1000) != HAL_OK)
+		return (1);
+
+	// Reading was succesfully read: copy the value
+	swapBytes(&returnData, data);
+	//*data /= 4;
+	//*data = returnData;
+
+	return 0;
+}
+
 int readSensors()
 {
 	readSensor(SI7021_ADDRESS, CMD_SI7021_HUMIDITY, &rawHumidity);
 	readSensor(SI7021_ADDRESS, CMD_SI7021_TEMPERATURE, &rawTemperature);
-	readSensor(BMP180_ADDRESS, CMD_BMP180_PRESSURE, &rawPressure);
+	readBMPSensor(BMP180_ADDRESS, CMD_BMP180_PRESSURE, &rawPressure);
 
 	return 0;
 }
